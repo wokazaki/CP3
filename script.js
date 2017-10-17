8f463510712224971b4cb3455329878f1078f5bb
wokazaki <wcokazaki@gmail.com>
2017-10-17 17:45:31 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,14 @@
   'use strict';
 angular.module('FilterInControllerModule', []).
   controller('FilterController', ['filterFilter', function FilterController(filterFilter) {
-    this.array = input;
+    this.array = [
+      {name: 'Tobias'},
+      {name: 'Jeff'},
+      {name: 'Brian'},
+      {name: 'Igor'},
+      {name: 'James'},
+      {name: 'Brad'}
+    ];
     this.filteredArray = filterFilter(this.array, 'a');
   }]);
 })(window.angular);
