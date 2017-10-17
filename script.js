66d298890badafdcb98b2bd0f47e24cc5344c086
wokazaki <wcokazaki@gmail.com>
2017-10-17 16:59:28 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,63 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <script src="angular.js"></script>
-  <script src="script.js"></script>
-</head>
+angular.module("mainModule", [])
+  .controller("mainController", function ($scope, $filter, dateFilter)
+  {
+    // Initialization
+    $scope.stringData = "Example string";
+    $scope.dateData = new Date();
+    $scope.numberData = 1350620.547;
 
-<body ng-app="mainModule">
-  <div ng-controller="mainController">
-    <h3>1. Simple filter</h3>
-    <strong>Original data:</strong> {{stringData}}<br />
-    <strong>Filtered data:</strong> {{stringData | uppercase}}<br />
-    <br />
-    <h3>2. Chained filters</h3>
-    <strong>Original data:</strong> {{stringData}}<br />
-    <strong>Filtered data:</strong> {{stringData | lowercase | limitTo:7}}<br />
-    <br />
-    <h3>3. Filter in JavaScript</h3>
-    <strong>Original data:</strong> {{dateData}}<br />
-    <strong>Filtered data (HTML):</strong> {{dateData | date:"dd MMM yyyy"}}<br />
-    <strong>Filtered data (JavaScript - alternative 1):</strong> {{formatDate1(dateData, "dd MMM yyyy")}}<br />
-    <strong>Filtered data (JavaScript - alternative 2):</strong> {{formatDate2(dateData, "dd MMM yyyy")}}<br />
-    <br />
-    <h3>4. Custom filter</h3>
-    <label><strong>Original data:</strong> <input type="text" ng-model="numberData" /></label><br />
-    <strong>Filtered data (default):</strong> {{numberData | customCurrency}}<br />
-    <strong>Filtered data (with parameters):</strong> {{numberData | customCurrency:"&euro;":",":".":2}}<br />
-  </div>
-</body>
-</html>
+    // Utility functions
+    $scope.formatDate1 = function (date, format) {
+      return $filter("date")(date, format);
+    };
+
+    $scope.formatDate2 = function (date, format) {
+      return dateFilter(date, format);
+    };
+  })
+  .filter("customCurrency", function (numberFilter)
+  {
+    function isNumeric(value)
+    {
+      return (!isNaN(parseFloat(value)) && isFinite(value));
+    }
+
+    return function (inputNumber, currencySymbol, decimalSeparator, thousandsSeparator, decimalDigits) {
+      if (isNumeric(inputNumber))
+      {
+        // Default values for the optional arguments
+        currencySymbol = (typeof currencySymbol === "undefined") ? "$" : currencySymbol;
+        decimalSeparator = (typeof decimalSeparator === "undefined") ? "." : decimalSeparator;
+        thousandsSeparator = (typeof thousandsSeparator === "undefined") ? "," : thousandsSeparator;
+        decimalDigits = (typeof decimalDigits === "undefined" || !isNumeric(decimalDigits)) ? 2 : decimalDigits;
+
+        if (decimalDigits < 0) decimalDigits = 0;
+
+        // Format the input number through the number filter
+        // The resulting number will have "," as a thousands separator
+        // and "." as a decimal separator.
+        var formattedNumber = numberFilter(inputNumber, decimalDigits);
+
+        // Extract the integral and the decimal parts
+        var numberParts = formattedNumber.split(".");
+
+        // Replace the "," symbol in the integral part
+        // with the specified thousands separator.
+        numberParts[0] = numberParts[0].split(",").join(thousandsSeparator);
+
+        // Compose the final result
+        var result = currencySymbol + numberParts[0];
+
+        if (numberParts.length == 2)
+        {
+          result += decimalSeparator + numberParts[1];
+        }
+
+        return result;
+      }
+      else
+      {
+        return inputNumber;
+      }
+    };
+  });
