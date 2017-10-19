e18c31745510ff30a947d40c0fde0967b7644ce2
alexandergillie <alexander.gillie@gmail.com>
2017-10-18 20:52:46 -0600

    Finished the cool stuff

--- test.js
+++ test.js
@@ -4,8 +4,8 @@ myApp.controller('BrideController', function($scope){
     $scope.y = 1000;
 
     $scope.runAway = function(){
-      var x = Math.random() * 400;
-      var y = Math.random() * 400;
+      var x = Math.random() * 1400;
+      var y = Math.random() * 700;
       console.log(x);
       console.log(y);
       console.log("Poop");
